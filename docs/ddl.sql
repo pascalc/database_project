996428dfe10cb47bc2910f431ccfe4c13a084572
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2010-10-25 18:33:17 +0800

    Added docs/ directory including the beginnings of a DDL file for our database schema.

---
+++ docs/ddl.sql
@@ -0,0 +1,30 @@
+USE dbproject ;
+
+-- -----------------------------------------------------
+-- Table dbproject.Users
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS dbproject.Users ;
+
+CREATE  TABLE IF NOT EXISTS dbproject.Users (
+  username VARCHAR(16) NOT NULL ,
+  password VARCHAR(16) NOT NULL ,
+  email VARCHAR(32) NOT NULL ,
+  creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (username) )
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table dbproject.Ads
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS dbproject.Ads;
+
+CREATE  TABLE IF NOT EXISTS dbproject.Ads (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(128) NOT NULL ,
+  description TEXT NOT NULL ,
+  category VARCHAR(32) NOT NULL ,
+  creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  fk_username VARCHAR(16),
+  FOREIGN KEY (fk_username) REFERENCES dbproject.Users(username),
+  PRIMARY KEY (id,fk_username))
+ENGINE = InnoDB;
