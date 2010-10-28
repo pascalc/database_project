0524004e0abe4809a7ef12370cb4a03c8a4af254
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2010-10-28 18:34:23 +0800

    Added login functionality.

--- app.rb
+++ app.rb
@@ -6,21 +6,23 @@ require 'erb'
 require 'sequel'
 require 'util.rb'
 
+enable :sessions
+
 # Set up the database connection
 
 DB = Sequel.mysql('dbproject',:host => "localhost", :user => "pascal", :password => "dbproject")
 
-# Routes
+########### ROUTES ###############
 
 # List all users
 get '/users/list' do
-  @users = DB["SELECT * FROM Users"]
+  @users = DB["SELECT * FROM Users ORDER BY creation_date DESC"]
   erb :users
 end
 
 # List all ads
 get '/ads/list' do 
-  @ads = DB["SELECT * FROM Ads"]
+  @ads = DB["SELECT * FROM Ads ORDER BY creation_date DESC"]
   erb :ads
 end
 
@@ -31,5 +33,25 @@ post '/new_ad' do
    category = params['category']
    DB["INSERT INTO Ads (id, title, description, category, creation_date, fk_username)
                 VALUES (null, ?, ?, ?, null, '1Yea')", title, description, category].insert
-   "Created a new ad!"
+   redirect '/ads/list'
+end
+
+# Create new user
+post '/new_user' do
+   username = params['username']
+   password = params['password']
+   email = params['email']
+   DB["INSERT INTO Users VALUES (?,?,?,null)",username,password,email].insert
+   redirect '/users/list'
+end
+
+# Log in
+post '/login' do
+   username = params['username']
+   password = params['password']
+   
+   result = DB["SELECT username,password FROM Users WHERE username = ? AND password = ?",username,password]
+   return "Don't try and hack me, #{username} = bad boy!" if result.empty?
+
+   "Welcome back #{username}!"
 end
