75ccb30219a4f7c21eb8ae753d36c75cc7a0c587
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2010-10-28 17:36:37 +0800

    Added create new ad route.

--- app.rb
+++ app.rb
@@ -12,17 +12,24 @@ DB = Sequel.mysql('dbproject',:host => "localhost", :user => "pascal", :password
 
 # Routes
 
+# List all users
 get '/users/list' do
   @users = DB["SELECT * FROM Users"]
   erb :users
 end
 
+# List all ads
 get '/ads/list' do 
   @ads = DB["SELECT * FROM Ads"]
   erb :ads
 end
 
+# Create a new ad
 post '/new_ad' do
-	"Created a new ad"
-	puts "Number of rows returned: #{res.num_rows}"
+   title = params['title']
+   description = params['description']
+   category = params['category']
+   DB["INSERT INTO Ads (id, title, description, category, creation_date, fk_username)
+                VALUES (null, ?, ?, ?, null, '1Yea')", title, description, category].insert
+   "Created a new ad!"
 end
