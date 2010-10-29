ad36597e7515979ff79e35866a6813fb381a3c8b
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2010-10-30 00:48:31 +0800

    Created 'info', 'warning', 'error' and 'success' classes of flash messages

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ end
 # Show new ad form
 get '/ads/new' do
    if not session["username"]
-      session["message"] = "Please log in first"
+      session["warning"] = "Please log in first"
       redirect '/'
    end
    erb :new_ad
@@ -48,17 +48,17 @@ post '/new_ad' do
    category = params['category']
    
    if title.length == 0 or description.length == 0 or category.length == 0
-        session["message"] = "Please enter a title, description and category."
+        session["error"] = "Please enter a title, description and category."
 	redirect '/ads/new'
    end
 
    begin 
    	DB["INSERT INTO Ads (id, title, description, category, creation_date, fk_username)
                 VALUES (null, ?, ?, ?, null, ?)", title, description, category,session["username"]].insert
-   	session["message"] = "Created a new ad!"
+   	session["success"] = "Created a new ad!"
    	redirect '/ads/list'
    rescue
-	session["message"] = "Something went wrong..."
+	session["error"] = "Something went wrong..."
      	redirect '/ads/new'
    end	
 end
@@ -68,7 +68,7 @@ get '/tag/:cat' do |cat|
    @tag = cat
    @ads = DB["SELECT * FROM Ads WHERE category = ? ORDER BY creation_date DESC",@tag]
    if @ads.empty?
-   	session["message"] = "We don't have a #{@tag} category."
+   	session["error"] = "We don't have a #{@tag} category."
         redirect '/ads/list'
    end	
    erb :ads 
@@ -86,19 +86,19 @@ post '/new_user' do
    email = params['email']
 
    if username.length == 0 or password.length == 0 or email.length == 0
-        session["message"] = "Please enter a username, password and email."
+        session["error"] = "Please enter a username, password and email."
 	redirect '/users/new'
    end
 
    begin 
    	DB["INSERT INTO Users VALUES (?,?,?,null)",username,password,email].insert
    rescue
-	session["message"] = "Sorry, we already have a user called #{username}."
+	session["error"] = "Sorry, we already have a user called #{username}."
 	redirect '/users/new'
    end
 
    session["username"] = username
-   session["message"] = "Welcome, #{username}!"
+   session["info"] = "Welcome, #{username}!"
    redirect '/users/list'
 end
 
@@ -109,12 +109,12 @@ post '/login' do
    
    result = DB["SELECT username,password FROM Users WHERE username = ? AND password = ?",username,password]
    if result.empty?
-      session["message"] = "Log in failed, please try again."
+      session["error"] = "Log in failed, please try again."
       redirect '/'
    end
 
    session["username"] = username
-   session["message"] = "Welcome back #{username}!"
+   session["info"] = "Welcome back #{username}!"
    redirect '/ads/list'
 end
 
@@ -122,6 +122,6 @@ end
 get '/logout' do
    username = session["username"]
    session["username"] = nil
-   session["message"] = "Goodbye #{username}!"
+   session["info"] = "Goodbye #{username}!"
    redirect '/'
 end
